20799fea449b6d9ed847f12870ed784c497968a1
JnKhB <huba@appmaintainers.com>
2021-08-11 15:07:47 +0000

    first steps of bridgetown migration

---
+++ Shuffle/public/js/main.js
@@ -0,0 +1,71 @@
+// Burger menus
+
+function setHunDefault(){
+	localStorage.setItem("languagePreference", "HUN");
+}
+function setEnDefault(){
+	localStorage.setItem("languagePreference", "ENG");
+	console.log("SET");
+}
+document.addEventListener('DOMContentLoaded', function() {
+	//language
+	var firstTime = localStorage.getItem("first_time");
+	var language = window.navigator.userLanguage || window.navigator.language;
+	if((localStorage.getItem("languagePreference") == null) && language == "hu-HU"){
+		if(!document.URL.includes("hu")){
+            window.location.href="/hu/";
+			localStorage.setItem("hunBrowser", new Date());
+        }
+	}
+	else if(localStorage.getItem("languagePreference") === "HUN"){
+		if(!document.URL.includes("hu")){
+            window.location.href="/hu/";
+		}
+	}
+	else if(localStorage.getItem("languagePreference") === "ENG"){
+		if(document.URL.includes("hu")){
+			console.log(document.URL);
+            window.location.href="../";
+		}
+	}
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+});
+
+
+
