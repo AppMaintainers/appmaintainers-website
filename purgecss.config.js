31c13968ff1fab242e8c8e8122d85b5982d8a852
JnKhB <huba@appmaintainers.com>
2021-09-08 13:55:20 +0000

    changes of purgecss config file

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   content: ['frontend/javascript/*.js','./output/**/*.html'],
-  output: "./output/_bridgetown/static/css"
+  output: "./output/_bridgetown/static/css",
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
 }
