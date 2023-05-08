cf803734c8482ed14fdff4ed1b56645ea180253d
Attila Juhász <goraffe@gmail.com>
2023-05-08 10:07:03 +0200

    Remove old site

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['frontend/javascript/*.js','./output/**/*.html'],
-  output: "./output/_bridgetown/static/css",
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
-}
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge')
-
-let config = require("./config/webpack.defaults.js")
-
-
-////////////////////////////////////////////////////////
-
-module.exports = config
